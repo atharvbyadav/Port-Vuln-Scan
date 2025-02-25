4c800476ba69ce39d055a231f9b9f84a2c574782
Atharv Yadav <atharvbyadav@gmail.com>
2025-02-25 10:11:41 +0530

    Error rich code

--- vulnscan.py
+++ vulnscan.py
@@ -4,6 +4,22 @@ import socket
 import os
 import sys
 
+def retBanner(ip,port):
+	try:
+		socket.setdefaulttimeout(2)
+		s = socket.socket()
+		s.connect((ip,port))
+		banner = s.recv(1024)
+		return banner
+	except:
+		return
+
+def checkVulns(banner, filename):
+	f = open(filename, "r")
+	for line in  f.readlines():
+		if line.strip("\n") in banner:
+			print("[+] Server is Vulnerable: " + banner.strip("\n"))
+
 def main():
 	if len(sys.argv) == 2:
 		filename = sys.argv[1]
@@ -16,7 +32,7 @@ def main():
 		print("[-] Usage: " + str(str.argv[0]) + "<vuln filename>")
 		exit(0)
 	portlist = [21, 22, 25, 80, 110, 443,445]
-	for x in range(4,6):
+	for x in range(4,7):
 		ip = "10.0.2." + str(x)
 		for port in portlist:
 			banner = retBanner(ip,port)
